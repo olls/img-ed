792afc88034d1bc0b4f45901a1ea24e0f20a1f8a
Oliver Faircliff <mail@oliverfaircliff.com>
2014-11-02 15:41:17 +0000

    More work on modal functionality, some buttons work now

--- src/js/main.js
+++ src/js/main.js
@@ -14,12 +14,20 @@ function on (e, obj, cb) {
 
 var img_ed = {};
 
+img_ed.defaults = {
+  width: 300,
+  height: 150
+};
+
 img_ed.controls = {
   load: {
     name: 'Load',
     func: function (e) {
-      img_ed.load_img('http://localhost/projects/dvbris.com/build/images/logo.png');
       console.log('Load');
+      img_ed.show(img_ed.load_modal);
+      on('modal_done', img_ed.load_modal, function () {
+        img_ed.hide(img_ed.load_modal);
+      });
     }
   },
   save: {
@@ -33,26 +41,95 @@ img_ed.controls = {
     func: function (e) {
       console.log('Pen');
     }
+  },
+  clear: {
+    name: 'Clear',
+    func: function (e) {
+      console.log('Clear')
+      img_ed.ctx.clearRect(0, 0, img_ed.canvas.width, img_ed.canvas.height);
+      img_ed.canvas.width = img_ed.defaults.width;
+      img_ed.canvas.height = img_ed.defaults.height;
+    }
   }
 };
 
-img_ed.add_controls = function (elem) {
-  var t = this;
-  Object.keys(t.controls).forEach(function (key) {
-    var control = t.controls[key];
+img_ed.load_controls = {
+  browse: {
+    name: 'Browse...',
+    func: function (e) {
+      console.log('Browse');
+    }
+  },
+  url: {
+    name: 'URL:',
+    type: 'text',
+    func: function (e, elems) {
+      console.log('URL');
+      img_ed.load_img(elems.text.value);
+    }
+  },
+  exit: {
+    name: 'Cancel',
+    func: function (e) {
+      console.log('Cancel');
+      img_ed.hide(img_ed.load_modal);
+    }
+  }
+};
+
+img_ed.show = function (modal) {
+  img_ed.lock = true;
+  $('body').classList.add('lock');
+  modal.classList.add('current');
+  modal.classList.remove('off');
+}
+
+img_ed.hide = function (modal) {
+  console.log('Hide')
+  img_ed.lock = false;
+  $('body').classList.remove('lock');
+  modal.classList.remove('current');
+  modal.classList.add('off');
+}
+
+img_ed.add_controls = function (elem, controls) {
+  Object.keys(controls).forEach(function (key) {
+    var control = controls[key];
     
-    var btn = document.createElement('button');
+    // Create Element(s)
+    var elems = {};
+
+    elems.cont = document.createElement('div');
+    elems.cont.classList.add('cont');
+
+    elems.btn = document.createElement('button');
+    elems.cont.appendChild(elems.btn);
+
+    if (control.type == 'text') {
+      elems.text = document.createElement('input');
+      elems.text.setAttribute('type', 'text');
+      elems.cont.appendChild(elems.text);
+    }
     
-    btn.innerHTML = control.name;
-    on('click', btn, (function (control) {
+    // Add Name
+    elems.btn.innerHTML = control.name;
+
+    // Add Function
+    on('click', elems.btn, (function (control, elems) {
       return function (e) {
-        control.func(e);
+        var modal = elems.cont.parentNode.parentNode;
+        var isin_modal = modal.classList.contains('modal');
+        // Only run if not locked or the buttons modal is specificly unlocked.
+        if (!img_ed.lock || (isin_modal && modal.classList.contains('current'))) {
+          control.func(e, elems);
+          modal.dispatchEvent(img_ed.modal_done);
+        }
         e.preventDefault();
         return false;
       };
-    })(control));
+    })(control, elems));
     
-    elem.appendChild(btn);
+    elem.appendChild(elems.cont);
   });  
 }
 
@@ -62,23 +139,29 @@ img_ed.load_img = function (img_s) {
   on('load', img, function (e) {
     t.canvas.width = img.width;
     t.canvas.height = img.height;
-    t.cxt.drawImage(img, 0, 0, img.width, img.height);
+    t.ctx.drawImage(img, 0, 0, img.width, img.height);
   });
   img.src = img_s;
 }
 
 img_ed.main = function () {
+  this.lock = false;
+  this.modal_done = new Event('modal_done');
+
   this.canvas = $('#img');
-  this.controls_e = $('#edit .controls');
+  this.edit_controls_e = $('#edit .controls');
+  this.load_modal = $('#load');
+  this.load_controls_e = $('#load .controls');
 
   // Test for canvas support
   if (!this.canvas.getContext) {
     return false;
   }
-  this.cxt = this.canvas.getContext('2d');
+  this.ctx = this.canvas.getContext('2d');
 
   // Add control buttons to DOM
-  this.add_controls(this.controls_e)
+  this.add_controls(this.edit_controls_e, this.controls);
+  this.add_controls(this.load_controls_e, this.load_controls);
 
 }
 
