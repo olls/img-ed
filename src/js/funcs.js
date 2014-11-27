c6c55957f9dd0666ac9d7be6f127eff60d768af6
Oliver Faircliff <mail@oliverfaircliff.com>
2014-11-27 16:12:47 +0000

    Added another border

--- src/js/funcs.js
+++ src/js/funcs.js
@@ -43,7 +43,8 @@ var img_ed = (function () {
       'images/border-1',
       'images/border-2',
       'images/border-3',
-      'images/border-4'
+      'images/border-4',
+      'images/border-5'
     ]
   };
 
